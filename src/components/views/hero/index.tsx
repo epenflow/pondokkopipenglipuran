612977f0d071db691303fb667c376c07133ec018
epenflow <epenflow@gmail.com>
2025-01-09 00:12:55 +0800

    refactoring hoc

--- src/components/views/hero/index.tsx
+++ src/components/views/hero/index.tsx
@@ -1,14 +1,8 @@
 'use client';
-import { useGSAP } from '@gsap/react';
-import { gsap, ScrollTrigger } from 'gsap/all';
 import React from 'react';
-
-gsap.registerPlugin(useGSAP, ScrollTrigger);
-
 import './base.css';
-interface Props {
-	scope: React.RefObject<HTMLElement | null>;
-}
+import hoc, { type Props } from './hoc';
+
 const Hero: React.FC<Props> = ({ scope }) => {
 	const CSSVariables = {
 		'--background-image':
@@ -26,88 +20,3 @@ const Hero: React.FC<Props> = ({ scope }) => {
 	);
 };
 export default hoc(Hero);
-function hoc<T extends object>(Component: React.ComponentType<T>) {
-	return function HOC(props: T) {
-		const scope = React.useRef<HTMLElement>(null);
-
-		// function createSquare(total: number) {
-		// 	const parent = document.createElement('div');
-		// 	parent.classList.add('hero-container--box');
-		// 	for (let index = 0; index < total; index++) {
-		// 		const element = document.createElement('div');
-		// 		element.classList.add('hero--box');
-		// 		parent.appendChild(element);
-		// 	}
-		// 	return parent;
-		// }
-
-		useGSAP(
-			() => {
-				gsap.to('.hero-container--image', {
-					filter: 'blur(0rem) ',
-					duration: 0.5,
-				});
-				gsap.to('.hero-container', {
-					ease: 'sine.inOut',
-					opacity: 0.75,
-					scale: 0.75,
-					scrollTrigger: {
-						pin: scope.current,
-						start: 'top top',
-						end: 'bottom+=100%',
-						scrub: 1.5,
-						markers: true,
-					},
-				});
-			},
-			{ scope },
-		);
-
-		// useGSAP(
-		// 	(_, contextSafe) => {
-		// 		if (!contextSafe) return null;
-		// 		const size = Number(getPropertyValue('--hero-box--size').match(/\d+/)?.[0]);
-		// 		const radius = getPropertyValue('--hero-radius');
-		// 		const { total } = getComputeGridDimensions(size);
-
-		// 		const appendChild = contextSafe(() => {
-		// 			scope.current?.appendChild(createSquare(total));
-		// 		});
-
-		// 		appendChild();
-
-		// 		const boxes: HTMLElement[] = gsap.utils.toArray('.hero--box');
-		// 		boxes.forEach((box) => {
-		// 			const mousemove = contextSafe(() => {
-		// 				gsap.to(box, {
-		// 					backgroundColor: 'blue',
-		// 					filter: 'blur(0.5rem)',
-		// 					borderRadius: radius,
-		// 					border: '20px solid blue',
-		// 				});
-		// 			});
-		// 			const mouseleave = contextSafe(() => {
-		// 				gsap.to(box, {
-		// 					backgroundColor: 'transparent',
-		// 					filter: 'blur(0rem)',
-		// 					borderRadius: 0,
-		// 					border: 'none',
-		// 				});
-		// 			});
-
-		// 			box.addEventListener('mousemove', mousemove);
-		// 			box.addEventListener('mouseleave', mouseleave);
-
-		// 			return () => {
-		// 				box.removeEventListener('mousemove', mousemove);
-		// 				box.removeEventListener('mouseleave', mouseleave);
-		// 			};
-		// 		});
-		// 		scope.current?.addEventListener('resize', appendChild);
-		// 		return () => scope.current?.removeEventListener('mouseleave', appendChild);
-		// 	},
-		// 	{ scope },
-		// );
-		return <Component {...{ ...props, scope }} />;
-	};
-}
