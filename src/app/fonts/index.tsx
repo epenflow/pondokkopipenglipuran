1d1c42a5f80ed90867b64d4d40a480dd2511ebb3
epenflow <epenflow@gmail.com>
2025-01-10 22:37:22 +0800

    feat: add subsets option for Inter and Bricolage fonts

--- src/app/fonts/index.tsx
+++ src/app/fonts/index.tsx
@@ -2,7 +2,9 @@ import { Bricolage_Grotesque, Inter } from 'next/font/google';
 
 export const inter = Inter({
 	variable: '--font-inter',
+	subsets: ['latin'],
 });
 export const bricolage = Bricolage_Grotesque({
 	variable: '--font-bricolage',
+	subsets: ['latin'],
 });
