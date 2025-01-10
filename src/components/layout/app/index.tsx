98fea8ca256f5924194a8cd678d3368a6aa342b0
epenflow <epenflow@gmail.com>
2025-01-10 23:21:57 +0800

    feat: add Header component and integrate it into the main App layout

--- src/components/layout/app/index.tsx
+++ src/components/layout/app/index.tsx
@@ -2,11 +2,17 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 
 const App: React.FC<React.PropsWithChildren> = ({ children }) => {
-	const { Lenis } = resources;
+	const { Lenis, Header } = resources;
 
-	return <Lenis frameRate={500}>{children}</Lenis>;
+	return (
+		<Lenis frameRate={500}>
+			<Header />
+			{children}
+		</Lenis>
+	);
 };
 const resources = {
 	Lenis: dynamic(() => import('~/components/layout/lenis')),
+	Header: dynamic(() => import('~/components/layout/header')),
 };
 export default App;
---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,11 @@
+import './base.css';
+const Header = () => {
+	return (
+		<header className="header-container">
+			<nav className="header-navbar">
+				<h1>Pondok Kopi</h1>
+			</nav>
+		</header>
+	);
+};
+export default Header;
